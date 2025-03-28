a40350adbcd86a68b8f29b151e8b5f594912650d
harshika <harshikaasiwal711@gmail.com>
2025-03-28 10:25:00 +0530

    Update Dark Mode Context

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,10 +4,7 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
-    "isDarkMode",
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
 
   useEffect(
     function () {
